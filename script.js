3ad694191d8fbd007d2f9b8ae56e8e562501e17b
younes-oss <aitchattouyounes@gmail.com>
2025-11-01 17:40:21 +0100

    remplir les options a la question approprie

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quiz = [
       { q: "Capitale de la France ?", 
-        options: ["Paris", "Rome", "Madrid"],
+        options: ["Paris", "Rome", "Madrid","Rabat"],
         correct: 0 
         },
 
@@ -12,12 +12,68 @@ const quiz = [
       {
         q: "Le soleil est une ?",
         options: ["Planète", "Étoile", "Lune"],
-         correct: 1 }
+         correct: 1 
+      }
     ];
 
     let index = 0;
     let score = 0;
 
-    let questionEL = document.getElementById("question");
-    let optionEl   =document.getElementById("option");
+    const questionEL = document.getElementById("question");
+    const optionContent = document.querySelectorAll(".ops");
+    const optionDivs = document.querySelectorAll(".option");
+    const scoreValue = document.querySelector(".score");
+    const result = document.getElementById("result");
+
     let current    = quiz[index];
+
+    function showQuestion() {
+    const current = quiz[index];
+    questionEL.textContent = current.q;
+
+  // vider les anciens textes
+  // optionContent.forEach(el => el.textContent = "");
+
+  // remplir les nouvelles options
+  current.options.forEach((opt, i) => {
+    optionContent[i].textContent = opt;
+  });
+  }
+
+
+
+ 
+  showQuestion();
+    
+    // Array.from(optionEl).forEach(elem =>{
+    //   console.log(elem);
+    //   elem.innerHTML = ''
+      
+    // });
+    
+
+    // optionContent.forEach(elem =>{
+    //   console.log(elem);
+    //   elem.innerHTML = ''
+      
+    // });
+
+    //  current.options.forEach((opt,i) => {
+    //       optionContent[i].innerHTML=opt; 
+    // });
+     
+     
+
+
+
+     
+      
+     
+
+    
+    
+
+    
+     
+
+
